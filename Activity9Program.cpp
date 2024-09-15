4a78d9e5e1297e37e7c0b16a4257323c5813d0a7
Zak <zakariabouaddou@gmail.com>
2024-09-15 12:12:34 -0700

    Add project files.

---
+++ Activity9Program.cpp
@@ -0,0 +1,17 @@
+// Activity 9: Program that check for job eligibility
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    // US citizen
+    // Bachelors Degree without any years of experience
+
+    bool US_Citizen = true;
+    bool BAdegree = true;
+    bool OtherExperience = false;
+
+    bool EligibleApplicant = US_Citizen == true && (BAdegree == true || OtherExperience == true);
+}
